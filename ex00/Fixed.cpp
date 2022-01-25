512f29c6530a19db3f787a5f5da283ba8a77e314
hgicquel <hgicquel@student.42.fr>
2022-01-25 12:13:24 +0100

    fixes

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: hgicquel <hgicquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 18:47:36 by hgicquel          #+#    #+#             */
-/*   Updated: 2022/01/19 12:43:14 by hgicquel         ###   ########.fr       */
+/*   Updated: 2022/01/25 12:12:02 by hgicquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 const int	Fixed::bits = 8;
 
 Fixed::Fixed(void):
-	value(0)
+	raw(0)
 {
 	std::cout << "Default constructor called" << "\n";
 }
@@ -29,22 +29,22 @@ Fixed::~Fixed(void)
 Fixed::Fixed(Fixed &from)
 {
 	std::cout << "Copy constructor called" << "\n";
-	this->value = from.getRawBits();
+	this->raw = from.getRawBits();
 }
 
 void Fixed::operator=(Fixed& from)
 {
 	std::cout << "Assignation operator called" << "\n";
-	this->value = from.getRawBits();
+	this->raw = from.getRawBits();
 }
 
 int	Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << "\n";
-	return (this->value);
+	return (this->raw);
 }
 
-void	Fixed::setRawBits(int value)
+void	Fixed::setRawBits(int const raw)
 {
-	this->value = value;
+	this->raw = raw;
 }
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hgicquel <hgicquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 18:42:58 by hgicquel          #+#    #+#             */
-/*   Updated: 2022/01/24 14:43:47 by hgicquel         ###   ########.fr       */
+/*   Updated: 2022/01/25 12:11:58 by hgicquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 class Fixed
 {
 	private:
-		int					value;
+		int					raw;
 		static const int	bits;
 	public:	
 		Fixed(void);
@@ -23,5 +23,5 @@ class Fixed
 		Fixed(Fixed& from);
 		void	operator=(Fixed& from);
 		int		getRawBits(void) const;
-		void	setRawBits(int value);
+		void	setRawBits(int const raw);
 };
